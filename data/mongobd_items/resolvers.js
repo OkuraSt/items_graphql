1b603f61c580b7ad31f7da6735def7d80366a881
Vedha Ivan Barrientos Pantoja <vedha.ivan@gmail.com>
2017-03-20 16:00:54 -0600

    Resolvers for create and update mutations

--- data/mongobd_items/resolvers.js
+++ data/mongobd_items/resolvers.js
@@ -9,4 +9,26 @@ export const resolvers = {
             });
         }
     },
+    Mutation: {
+        updateItem(_, {id, title, description}){
+            Item.findById(id, function (err, item) {
+                if (err) return null;
+
+                item.title = title;
+                item.description = description;
+                item.save(function (err, updated) {
+                    return err ? null : updated ;
+                });
+            });
+        },
+        createItem(_, {input}, ){
+            return Promise.resolve()
+                .then(() => {
+                    return Item.create({title: input.title, description: input.description});
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        }
+    },
 };
